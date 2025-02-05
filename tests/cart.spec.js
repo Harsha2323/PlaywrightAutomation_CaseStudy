02f061c5bb126944788bfe66548a0d47f40c8505
harsha <harshanavale23@gmail.com>
2025-02-05 23:15:37 +0530

    tesst

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -17,7 +17,7 @@ test('Verify User is able to view the delete the added product', async ({page}) 
   await cartpage.verifyingitem()
   await page.screenshot({ path:'tests/Screenshot/'+Date.now()+'PoductsAddedInCart.png', fullPage: true})
   await cartpage.deletefromcart()
-  console.log("navale  harsha")
+  console.log("navale k harsha")
 });
 
 test('Verify User is able to update the quality of the product and delete the product', async ({page}) => {
