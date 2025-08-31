7aba3af0053eb8773960a8cdd274107ada373096
harsha <harshanavale23@gmail.com>
2025-08-31 18:06:40 +0530

    tester1

---
+++ tests/test1.spec.js
@@ -0,0 +1,10 @@
+const {test, expect} = require('@playwright/test')
+
+
+test('demo1',async ({page})=>{
+
+await page.goto('https://www.youtube.com/watch?v=JgDNFQ2RaLQ&list=RDn2dVFdqMYGA&index=3')
+await page.goBack()
+await page.goForward()
+console.log('this is YOUTUBE')
+})
