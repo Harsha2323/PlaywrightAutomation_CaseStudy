0bbcbd3beedca897025f4bc464b4e7f89c9e8b11
harsha <harshanavale23@gmail.com>
2025-08-31 18:04:28 +0530

    tester2update

---
+++ tests/test2.spec.js
@@ -0,0 +1,10 @@
+const {test, expect} = require('@playwright/test')
+
+
+test('demo1',async ({page})=>{
+
+await page.goto('https://cetonline.karnataka.gov.in/kea/ugcet2025')
+await page.goBack()
+await page.goForward()
+console.log('this is KEA')
+})
