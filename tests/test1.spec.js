8c31f7dc0523737f7be709d52abe0bf80c87b70d
harsha <harshanavale23@gmail.com>
2025-08-31 18:08:29 +0530

    tester new change

--- tests/test1.spec.js
+++ tests/test1.spec.js
@@ -6,5 +6,5 @@ test('demo1',async ({page})=>{
 await page.goto('https://www.youtube.com/watch?v=JgDNFQ2RaLQ&list=RDn2dVFdqMYGA&index=3')
 await page.goBack()
 await page.goForward()
-console.log('this is YOUTUBE')
+console.log('this is KEA')
 })
